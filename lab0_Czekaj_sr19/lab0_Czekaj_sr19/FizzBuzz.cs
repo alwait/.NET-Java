f6ba3a7cbd09c67df209da4a33ff6800cdb9dc3e
alwait <161856227+alwait@users.noreply.github.com>
2024-03-01 16:06:51 +0100

    Add files via upload

---
+++ lab0_Czekaj_sr19/lab0_Czekaj_sr19/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0_Czekaj_sr19
+{
+    internal class FizzBuzz
+    {
+        int uB;
+        public FizzBuzz(int ub)
+        {
+            uB = ub;
+        }
+        public void Display()
+        {
+            for(int i = 1; i <= uB; i++) 
+            {
+                if(i % 3 != 0 && i % 5 != 0) 
+                {
+                    Console.Write(i);
+                }
+                if(i%3==0)
+                {
+                    Console.Write("Fizz");
+                }
+                if(i%5==0) 
+                {
+                    Console.Write("Buzz");
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ lab0_Czekaj_sr19/lab0_Czekaj_sr19/Program.cs
@@ -0,0 +1,4 @@
+﻿using lab0_Czekaj_sr19;
+
+FizzBuzz fz1 = new FizzBuzz(20);
+fz1.Display();
